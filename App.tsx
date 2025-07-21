38b34fcde8289760adc2e5c02705d4cabb2b177e
João Pedro <jpedro-lima@outlook.com>
2025-07-21 14:18:25 -0300

    add gluestack UI with button and alert-dialog

--- App.tsx
+++ App.tsx
@@ -2,12 +2,17 @@ import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
+import { GluestackUIProvider } from '@/ui/gluestack-ui-provider';
+import { ExampleGluestack } from '@/ExampleGluestack';
 
 export default function App() {
   return (
     <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
+      <GluestackUIProvider>
+        <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+        <ExampleGluestack />
+        <StatusBar style="auto" />
+      </GluestackUIProvider>
     </>
   );
 }
