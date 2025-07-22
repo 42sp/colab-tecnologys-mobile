1119cc993aa47779fadcf54adc23f15f4c736daf
João Pedro <jpedro-lima@outlook.com>
2025-07-22 20:33:59 -0300

    add: expo router Foi criado as pastas src e app para mapeamento de rotas

---
+++ src/app/home.tsx
@@ -0,0 +1,16 @@
+import { useRouter } from 'expo-router';
+import { Button, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function Home() {
+  const router = useRouter();
+
+  return (
+    <SafeAreaView>
+      <Text className="flex self-center text-xl">Home</Text>
+      <View className="h-full items-center justify-center">
+        <Button title="Go to Sign-in" onPress={() => router.navigate('/sign-in')} />
+      </View>
+    </SafeAreaView>
+  );
+}
