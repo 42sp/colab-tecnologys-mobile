33e1a93c29d971a9af03243f311087ca8a27c57a
Phil <dev@lipe.ph>
2025-07-25 16:18:01 -0300

    feat: integrate Redux for state management with profile example

---
+++ redux/slices/profileSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { ProfileState, ProfileType} from "../types/profile"; // Adjust the import path as necessary
+
+  const initialState: ProfileState = {
+      name: "42 São Paulo",
+      age: 25,
+    };
+
+const profileSlice = createSlice({
+  name: "profile",
+  initialState,
+  reducers: {
+    setName(state, action: { payload: ProfileState['name'] }) {
+      state.name = action.payload;
+    },
+    setAge(state, action: { payload: ProfileState['age'] }) {
+      state.age = action.payload;
+    },
+  },
+} as ProfileType);
+
+export const { setName, setAge } = profileSlice.actions;
+export default profileSlice.reducer;
