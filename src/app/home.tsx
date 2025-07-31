2dd44611ef2bc377ead6bd7a101cb4fa2263fa86
João Pedro <jpedro-lima@outlook.com>
2025-07-31 13:49:33 -0300

    feat(sign-in): add scroll view functionality

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'expo-router'
 import { Text, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 import { Button } from '@/components/ui/button'
 import ProfileViewer from '@/components/ProfileViewer'
 
@@ -8,28 +8,30 @@ export default function Home() {
 	const router = useRouter()
 
 	return (
-		<SafeAreaView className="bg-white p-10">
-			<Text className="flex self-center text-xl">Home</Text>
-			<View className="h-full items-center justify-center gap-2">
-				<ProfileViewer />
-				<Button title="Sign-in" onPress={() => {}} />
+		<SafeAreaProvider>
+			<SafeAreaView className="w-full bg-white p-10">
+				<Text className="flex self-center text-xl">Home</Text>
+				<View className="h-full items-center justify-center gap-2">
+					<ProfileViewer />
+					<Button title="Sign-in" onPress={() => {}} />
 
-				<Button
-					title="Sign-in"
-					onPress={() => {
-						console.log('press google sign in')
-					}}
-					variant="outline"
-				/>
+					<Button
+						title="Sign-in"
+						onPress={() => {
+							console.log('press google sign in')
+						}}
+						variant="outline"
+					/>
 
-				<Button
-					title="Forgot password"
-					onPress={() => router.navigate('/forgot-password')}
-					variant="red"
-				/>
+					<Button
+						title="Forgot password"
+						onPress={() => router.navigate('/forgot-password')}
+						variant="red"
+					/>
 
-				<Button title="Go to Sign-in" onPress={() => router.navigate('/sign-in')} />
-			</View>
-		</SafeAreaView>
+					<Button title="Go to Sign-in" onPress={() => router.navigate('/sign-in')} />
+				</View>
+			</SafeAreaView>
+		</SafeAreaProvider>
 	)
 }
--- src/app/sign-in.tsx
+++ src/app/sign-in.tsx
@@ -1,31 +1,48 @@
-import { Image, Text, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { Image, Keyboard, Text, View } from 'react-native'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 import { SignInDivisor } from '@/components/sign-in/sign-in-divisor'
 import { Button } from '@/components/ui/button'
 import { SignInForm } from '@/components/sign-in/sign-in-form'
+import { useEffect, useState } from 'react'
+import { ScrollView } from 'react-native-gesture-handler'
 
 export default function SignIn() {
+	const [keyboardIsVisible, setKeyboardIsVisible] = useState(false)
+
+	useEffect(() => {
+		Keyboard.addListener('keyboardDidShow', () => {
+			setKeyboardIsVisible(true)
+		})
+		Keyboard.addListener('keyboardDidHide', () => {
+			setKeyboardIsVisible(false)
+		})
+	}, [])
+
 	return (
-		<SafeAreaView className="h-full bg-white p-10">
-			<Image source={require('@/assets/tecnologys-logo.png')} className="my-5 self-center" />
+		<ScrollView showsVerticalScrollIndicator={false} className="bg-white">
+			<SafeAreaProvider>
+				<SafeAreaView className={`h-full bg-white p-10 ${keyboardIsVisible ? 'mb-60' : ''}`}>
+					<Image source={require('@/assets/tecnologys-logo.png')} className="my-5 self-center" />
 
-			<View className="my-5 items-center">
-				<Text className="font-inter-bold text-3xl">Welcome Back</Text>
-				<Text className="mt-2 font-inter text-lg text-neutral-500">Sign in to continue</Text>
-			</View>
+					<View className="my-5 items-center">
+						<Text className="font-inter-bold text-3xl">Welcome Back</Text>
+						<Text className="mt-2 font-inter text-lg text-neutral-500">Sign in to continue</Text>
+					</View>
 
-			<Button
-				title="Continue with Google"
-				onPress={() => {}}
-				variant="outline"
-				className="my-5 self-center"
-			>
-				<Image source={require('@/assets/google-logo.png')} className="mr-2" />
-			</Button>
+					<Button
+						title="Continue with Google"
+						onPress={() => {}}
+						variant="outline"
+						className="my-5 self-center"
+					>
+						<Image source={require('@/assets/google-logo.png')} className="mr-2" />
+					</Button>
 
-			<SignInDivisor text="or" className="my-5" />
+					<SignInDivisor text="or" className="my-5" />
 
-			<SignInForm />
-		</SafeAreaView>
+					<SignInForm />
+				</SafeAreaView>
+			</SafeAreaProvider>
+		</ScrollView>
 	)
 }
