955e9f682d41c7cb1929cda4d7c507162118390b
João Pedro <jpedro-lima@outlook.com>
2025-08-04 14:52:59 -0300

    add: card component and button variant

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,22 +1,22 @@
 import React, { ReactNode } from 'react'
-import { Text, View, ViewProps } from 'react-native'
+import { View, ViewProps } from 'react-native'
 
 type CardBaseProps = {
 	children: ReactNode
 	className?: string
 } & ViewProps
 
-type CardComponent = React.FC<{ children: ReactNode }> & {
+type CardComponent = React.FC<CardBaseProps> & {
 	Header: React.FC<CardBaseProps>
 	Body: React.FC<CardBaseProps>
 	Footer: React.FC<CardBaseProps>
 }
 
-const Card: CardComponent = ({ children }) => {
-	return <View className=" rounded-lg bg-white p-5">{children}</View>
+const Card: CardComponent = ({ children, className }) => {
+	return <View className={`rounded-lg bg-white p-5 ${className}`}>{children}</View>
 }
 
-Card.Header = ({ children, className = '' }) => <View className={className}>{children}</View>
+Card.Header = ({ children, className }) => <View className={className}>{children}</View>
 
 Card.Body = ({ children, className }) => <View className={className}>{children}</View>
 Card.Footer = ({ children, className }) => <View className={className}>{children}</View>
