58b50d437ce1e1302cae8dce40ac11d6430a6874
João Pedro <jpedro-lima@outlook.com>
2025-08-04 13:59:08 -0300

    add: avatar component to profile

---
+++ src/components/profile/profile-avatar.tsx
@@ -0,0 +1,34 @@
+import { Image, Text, View } from 'react-native'
+import { Button } from '../ui/button'
+import { Feather } from '@expo/vector-icons'
+
+import avatarURL from '@/assets/avatar.png'
+
+type ProfileAvatarProps = {
+	avatar: string
+	name: string
+	email: string
+}
+
+export function ProfileAvatar({ avatar, name, email }: ProfileAvatarProps) {
+	return (
+		<View className="items-center">
+			<View className="size-36 rounded-full bg-white p-1 shadow-2xl">
+				<Image source={avatarURL} className="h-full w-full rounded-full" />
+				<Button variant="rounded" className="mt-[-35px] size-10 self-end">
+					<Feather
+						name="edit"
+						color={'#ffff'}
+						size={14}
+						onPress={() => console.log('avatar pressionado')}
+					/>
+				</Button>
+			</View>
+
+			<View className="my-3 items-center">
+				<Text className="font-inter-bold text-3xl">{name}</Text>
+				<Text className="text-center font-inter text-xl text-neutral-500">{email}</Text>
+			</View>
+		</View>
+	)
+}
