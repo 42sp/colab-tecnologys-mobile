61b5ccc7d45f983395c36ddee1e87f1ae96796dc
João Pedro <jpedro-lima@outlook.com>
2025-08-19 15:13:20 -0300

    feat(filter): add modal filter

--- src/screens/drawer/home/filter-modal.tsx
+++ src/screens/drawer/home/filter-modal.tsx
@@ -1,25 +1,20 @@
 import { Modal, View, Text, ScrollView, TouchableOpacity, Pressable } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 import { Button } from '@/components/ui/button'
-import { useForm, Controller } from 'react-hook-form'
 import { FilterType, StatusType } from './home'
-import CustomCalendar from '@/components/ui/calendar'
+import { CustomCalendar, DateRangeType } from '@/components/ui/calendar'
+import { useEffect, useState } from 'react'
 
 type FilterModalProps = {
 	isVisible: boolean
 	onClose: () => void
-	filter: FilterType | undefined
-	setFilter: React.Dispatch<React.SetStateAction<FilterType | undefined>>
+	filter: FilterType
+	setFilter: React.Dispatch<React.SetStateAction<FilterType>>
 }
 
 export function HomeFilterModal({ isVisible, onClose, setFilter, filter }: FilterModalProps) {
-	function handleFilterChange(newFilter: Partial<FilterType>) {
-		setFilter((prev) => ({ ...prev, ...newFilter }))
-	}
-
-	function resetFilters() {
-		handleFilterChange({ dateRange: { start: new Date(), end: new Date() }, status: [] })
-	}
+	const [dateRange, setDateRange] = useState<DateRangeType>({ start: null, end: null })
+	const [hiddenCalendar, setHiddenCalendar] = useState(true)
 
 	const statusLabels: Record<StatusType, string> = {
 		pending: 'Pendente',
@@ -27,22 +22,33 @@ export function HomeFilterModal({ isVisible, onClose, setFilter, filter }: Filte
 		completed: 'Concluído',
 	}
 
+	function onResetFilter() {
+		setDateRange({ start: null, end: null })
+		setFilter({
+			serviceType: 'Todos',
+			status: [],
+			dateRange: { start: null, end: null },
+		})
+	}
+
+	useEffect(() => {
+		if (dateRange.end) setHiddenCalendar(true)
+		setFilter((prev) => ({ ...prev, dateRange }))
+	}, [dateRange.end])
+
 	return (
 		<Modal visible={isVisible} animationType="slide" transparent>
-			<Pressable className="flex-1" onPress={() => onClose()}>
-				<View className="rounded-t-2xl bg-white p-4">
-					<View className="mb-4 flex-row items-center justify-between">
+			<Pressable className="flex-1 justify-end bg-black/15" onPress={onClose}>
+				<View className="gap-2 rounded-t-2xl bg-white p-4">
+					<View className="flex-row items-center justify-between">
 						<Text className="font-inter-bold text-xl">Filter by</Text>
-						<TouchableOpacity activeOpacity={0.5} onPress={() => resetFilters()}>
+						<TouchableOpacity activeOpacity={0.5} onPress={() => onResetFilter()}>
 							<Text className="p-4 font-inter text-blue-500">Reset All</Text>
 						</TouchableOpacity>
 					</View>
 
-					<View className="flex-1">
-						<View className="mb-2 flex-row items-center justify-between">
-							<Text className="mb-2 font-inter-medium">Status</Text>
-						</View>
-
+					<View className="gap-2">
+						<Text className="font-inter-medium">Status</Text>
 						<View className="flex-row gap-2">
 							{(['pending', 'approved', 'completed'] as StatusType[]).map((status) => {
 								const isSelected = filter?.status?.includes(status)
@@ -69,8 +75,24 @@ export function HomeFilterModal({ isVisible, onClose, setFilter, filter }: Filte
 							})}
 						</View>
 					</View>
-					<View className="flex-1">
-						<CustomCalendar />
+
+					<Pressable
+						onPress={() => (hiddenCalendar ? setHiddenCalendar(false) : setHiddenCalendar(true))}
+					>
+						<Text className="mb-2 font-inter-medium">Date Range</Text>
+						<View className="flex-row gap-2 space-x-2">
+							<View className="flex-1 flex-row items-center justify-between rounded-lg border p-2">
+								<Text>{dateRange.start ? dateRange.start.toLocaleDateString('pt-BR') : ''}</Text>
+								<Feather name="calendar" size={16} />
+							</View>
+							<View className="flex-1 flex-row items-center justify-between rounded-lg border p-2">
+								<Text>{dateRange.end ? dateRange.end.toLocaleDateString('pt-BR') : ''}</Text>
+								<Feather name="calendar" size={16} />
+							</View>
+						</View>
+					</Pressable>
+					<View className={`${hiddenCalendar ? 'hidden' : ''}`}>
+						<CustomCalendar setDateRange={setDateRange} />
 					</View>
 				</View>
 			</Pressable>
