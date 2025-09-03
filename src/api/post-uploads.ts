9d5b5ef29e2d8b005f93d3226f4fef6a827a8c53
sabrina <sabrinaferreiran@gmail.com>
2025-09-03 13:45:56 -0300

    fix(hooks): fixed use-axios import:  hook -> hooks

--- src/api/post-uploads.ts
+++ src/api/post-uploads.ts
@@ -1,24 +1,21 @@
-import useAxios from '@/hook/use-axios';
+import useAxios from '@/hooks/use-axios'
 
 interface UploadResponse {
-	id: string;
-	uri: string;
-	size: number;
-	contentType: string;
+	id: string
+	uri: string
+	size: number
+	contentType: string
 }
 
 export const usePostUploads = () => {
-	const res = useAxios<UploadResponse>(
-	  { url: 'uploads', method: 'post' },
-	  { manual: true }
-	)
+	const res = useAxios<UploadResponse>({ url: 'uploads', method: 'post' }, { manual: true })
 
 	const upload = res.fetchData!
 
 	return {
-	  upload,
-	  data: res.data,
-	  loading: res.loading,
-	  error: res.error,
+		upload,
+		data: res.data,
+		loading: res.loading,
+		error: res.error,
 	}
-  }
+}
