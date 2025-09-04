b96d17c19e88689a2e78b15c01c0fa23b7ec7b88
daniel <daniel101surf@gmail.com>
2025-09-04 14:19:40 -0300

    feat: add ResetPassword screen and integrate into navigation stack

--- src/_layouts/stack/stack.tsx
+++ src/_layouts/stack/stack.tsx
@@ -5,6 +5,7 @@ import SignInScreen from '@/screens/stack/sign-in/sign-in'
 import SignUpScreen from '@/screens/stack/sign-up/sign-up'
 import ForgotPasswordScreen from '@/screens/stack/forgot-password/forgot-password'
 import VerifyCode from '@/screens/stack/verify-code/verify-code'
+import ResetPassword from '@/screens/stack/reset-password/reset-password'
 import DrawerLayout, { DrawerParamList } from '@/_layouts/drawer/drawer'
 import { StackHeader } from './stack-header'
 
@@ -17,6 +18,7 @@ export type StackParamList = {
 	signUp: undefined
 	forgotPassword: undefined
 	verifyCode: undefined
+	resetPassword: undefined
 	drawer: NavigatorScreenParams<DrawerParamList>
 }
 
@@ -46,7 +48,16 @@ export default function StackLayout() {
 				component={VerifyCode}
 				options={{
 					headerShown: true,
-					title: 'New Account',
+					title: 'Segurança',
+					header: StackHeader,
+				}}
+			/>
+			<Stack.Screen
+				name="resetPassword"
+				component={ResetPassword}
+				options={{
+					headerShown: true,
+					title: 'Segurança',
 					header: StackHeader,
 				}}
 			/>
---
+++ src/screens/stack/reset-password/reset-password.tsx
@@ -0,0 +1,14 @@
+
+import { SafeAreaView, View } from 'react-native';
+
+export default function ResetPassword() {
+	return (
+		<SafeAreaView>
+			<View className="m-4 bg-white p-10 rounded-lg">
+				<View className="my-4 px-4">
+
+				</View>
+			</View>
+		</SafeAreaView>
+	)
+}
