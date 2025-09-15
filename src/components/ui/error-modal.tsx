0150dae7cd28e63eade53db4028a1d568680a15a
sabrina <sabrinaferreiran@gmail.com>
2025-09-15 19:59:56 -0300

    feat(modals): implement fade background modal in error, logout and home-filter modals

--- src/components/ui/error-modal.tsx
+++ src/components/ui/error-modal.tsx
@@ -1,6 +1,8 @@
 import { Modal, Text, View, TouchableOpacity } from 'react-native'
+import { useEffect, useState } from 'react'
 import { Feather } from '@expo/vector-icons'
 import Card from './card'
+import { FadeBackgroundModal } from './fade-background-modal'
 
 type ErrorModalProps = {
 	visible: boolean
@@ -10,20 +12,36 @@ type ErrorModalProps = {
 }
 
 export function ErrorModal({ visible, message, description, onClose }: ErrorModalProps) {
+	const [showCard, setShowCard] = useState(visible)
+	useEffect(() => {
+		if (visible) setShowCard(true)
+	}, [visible])
+
+	const handleFadeOut = () => {
+		if (!visible) setShowCard(false)
+	}
+
+	if (!showCard) return null
+
 	return (
-		<Modal animationType="slide" transparent={true} visible={visible} onRequestClose={onClose}>
-			<View className="w-full flex-1 items-center justify-end bg-black/40 p-10">
-				<Card className="w-full ">
-					<View className="flex-row justify-between ">
-						<Text className="font-inter-bold text-lg text-red-700">{message}</Text>
-						<TouchableOpacity onPress={onClose} hitSlop={10}>
-							<Feather name="x" size={20} color="#333" />
-						</TouchableOpacity>
+		<Modal animationType="none" transparent={true} visible={showCard} onRequestClose={onClose}>
+			<View className="flex-1 justify-end">
+				<FadeBackgroundModal visible={visible} onHidden={handleFadeOut} />
+				{visible && (
+					<View className="p-10">
+						<Card className="w-full">
+							<View className="flex-row justify-between">
+								<Text className="font-inter-bold text-lg text-red-700">{message}</Text>
+								<TouchableOpacity onPress={onClose} hitSlop={10}>
+									<Feather name="x" size={20} color="#333" />
+								</TouchableOpacity>
+							</View>
+							{description && (
+								<Text className="text-md font-inter text-gray-700">{description}</Text>
+							)}
+						</Card>
 					</View>
-					{description ? (
-						<Text className="text-md font-inter text-gray-700">{description}</Text>
-					) : null}
-				</Card>
+				)}
 			</View>
 		</Modal>
 	)
--- src/components/ui/logout-modal.tsx
+++ src/components/ui/logout-modal.tsx
@@ -2,6 +2,8 @@ import { Modal, View, Text, TouchableOpacity } from 'react-native'
 import { Button } from './button'
 import Card from './card'
 import { Feather } from '@expo/vector-icons'
+import { useEffect, useState } from 'react'
+import { FadeBackgroundModal } from './fade-background-modal'
 
 interface LogoutModalProps {
 	visible: boolean
@@ -10,31 +12,45 @@ interface LogoutModalProps {
 }
 
 export function LogoutModal({ visible, onClose, onConfirm }: LogoutModalProps) {
+	const [showCard, setShowCard] = useState(visible)
+	useEffect(() => {
+		if (visible) setShowCard(true)
+	}, [visible])
+
+	const handleFadeOut = () => {
+		if (!visible) setShowCard(false)
+	}
+
+	if (!showCard) return null
+
 	return (
-		<Modal animationType="slide" transparent={true} visible={visible} onRequestClose={onClose}>
+		<Modal animationType="none" transparent={true} visible={showCard} onRequestClose={onClose}>
 			<View className="flex-1 items-center justify-end bg-black/40">
-				<Card className="m-10 w-10/12">
-					<View className="flex-row justify-between ">
-						<Text className="font-inter-bold text-lg text-red-700">Sair</Text>
-						<TouchableOpacity onPress={onClose} hitSlop={10}>
-							<Feather name="x" size={20} color="#333" />
-						</TouchableOpacity>
-					</View>
-					<Text className="text-md font-inter text-gray-700">
-						Tem certeza que deseja fazer logout?
-					</Text>
-					<View className="flex-row justify-between p-2">
-						<Button title="Não" variant="pill" className="w-24 bg-red-100" onPress={onClose} />
-						<Button
-							title="Sim"
-							variant="pill"
-							className="w-24 flex-row items-center gap-2 bg-green-100"
-							onPress={onConfirm}
-						>
-							<Feather name="log-out" size={14} />
-						</Button>
-					</View>
-				</Card>
+				<FadeBackgroundModal visible={visible} onHidden={handleFadeOut} />
+				{visible && (
+					<Card className="m-10 w-10/12">
+						<View className="flex-row justify-between ">
+							<Text className="font-inter-bold text-lg text-red-700">Sair</Text>
+							<TouchableOpacity onPress={onClose} hitSlop={10}>
+								<Feather name="x" size={20} color="#333" />
+							</TouchableOpacity>
+						</View>
+						<Text className="text-md font-inter text-gray-700">
+							Tem certeza que deseja fazer logout?
+						</Text>
+						<View className="flex-row justify-between p-2">
+							<Button title="Não" variant="pill" className="w-24 bg-red-100" onPress={onClose} />
+							<Button
+								title="Sim"
+								variant="pill"
+								className="w-24 flex-row items-center gap-2 bg-green-100"
+								onPress={onConfirm}
+							>
+								<Feather name="log-out" size={14} />
+							</Button>
+						</View>
+					</Card>
+				)}
 			</View>
 		</Modal>
 	)
