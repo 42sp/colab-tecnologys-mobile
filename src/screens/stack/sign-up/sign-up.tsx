742cfc8aeaaf05785437860afcdf3e749e50bbca
sabrina <sabrinaferreiran@gmail.com>
2025-09-19 18:33:25 -0300

    add platform.os to signup keyboard view

--- src/screens/stack/sign-up/sign-up.tsx
+++ src/screens/stack/sign-up/sign-up.tsx
@@ -1,11 +1,11 @@
-import { KeyboardAvoidingView } from 'react-native'
+import { KeyboardAvoidingView, Platform } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { ScrollView } from 'react-native-gesture-handler'
 import { SignUpForm } from './sign-up-form'
 
 export default function SignUpScreen() {
 	return (
-		<KeyboardAvoidingView behavior={'height'}>
+		<KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
 			<SafeAreaView className="bg-white px-10">
 				<ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
 					<SignUpForm />
