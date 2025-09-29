7441f2560589caa8e37bfcd76be851dca2afd03f
Phil <dev@lipe.ph>
2025-09-29 10:00:23 -0300

    chore: expired token function

--- src/libs/axios/axios.ts
+++ src/libs/axios/axios.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 import store from '../redux/store'
 import { API_URL } from '@env'
 import { match, P } from 'ts-pattern'
+import { logoutUser } from '@/utils'
 
 export const api = axios.create({
 	baseURL: API_URL,
@@ -47,13 +48,17 @@ api.interceptors.response.use(
 		return response
 	},
 	(error) => {
-		console.log('[AXIOS][RESPONSE][ERROR]:', error)
+		console.log('[AXIOS][RESPONSE][ERROR]:', error.response.data.message)
 		if (error.response) {
 			const info = error.response.data.message || error.response.data.error || error.message
 			match({ info })
 				.with({ info: P.when((c: any) => c.indexOf('duplicate key value') !== -1) }, (e) =>
 					console.log('Erro de chave duplicada detectado:', e),
 				)
+				.with({ info: P.when((c: any) => c.indexOf('jwt expired') !== -1) }, (e) => {
+					console.log('Erro de token expirado detectado:', e)
+					logoutUser(store.dispatch)
+				})
 				.otherwise(() => false)
 			//console.log('[AXIOS][RESPONSE][ERROR][DATA]:', error.response.data)
 			console.log('[AXIOS][RESPONSE][ERROR][STATUS]:', error.response.status)
