6558ea0de29ca145c7096515471206c398608ca8
BrianFs <brianfsilvacode@outlook.com>
2025-10-18 17:56:13 -0400

    Criar nova tela de produtividade e andamento da obra

--- src/_layouts/tabs/tabs.tsx
+++ src/_layouts/tabs/tabs.tsx
@@ -1,13 +1,13 @@
-// import './style.css';
-// import { Tabs } from 'expo-router';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import RegisterServiceScreen from '@/screens/drawer/register-service/register-service';
 import Home from '@/screens/drawer/home/home';
 import { HomeIcon, Plus, UserIcon } from 'lucide-react-native';
 import { Text, View } from 'react-native';
 import ProfileScreen from '@/screens/drawer/profile/profile';
-import { useEffect } from 'react';
 import TabsHeader from './tab-header';
+import Dashboard from '@/screens/stack/home/dashboard';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/libs/redux/store';
 
 export type TabParamList = {
 	home: undefined
@@ -19,14 +19,10 @@ export type TabParamList = {
 	productivity: undefined
 }
 
-const triggerSeconds = 60 * 60;
-
 const Tab = createBottomTabNavigator<TabParamList>();
 
 const TabsLayout = () => {
-
-
-
+	const role = useSelector((state: RootState) => state.roles);
 
 	return (
 		<Tab.Navigator
@@ -36,31 +32,36 @@ const TabsLayout = () => {
 		>
 			<Tab.Screen
 				name="home"
-				component={Home}
+				component={
+					['executor', 'encarregado'].includes(role.role_name)
+					? Home : Dashboard }
 				options={{
 					title: 'Home',
 					tabBarIcon: ({ color, size }) => <HomeIcon color={color} size={size} />,
 					header: TabsHeader
 				}}
 			/>
-			<Tab.Screen
-				name="registerService"
-				component={RegisterServiceScreen}
-				options={{
-					title: 'Registrar',
-					tabBarLabel: ({ color, focused }) => focused ? '' : <Text style={{ color, fontSize: 10 }}>Registrar</Text>,
-					tabBarIcon: ({ color, size, focused }) => <View
-						className={`${focused ? 'w-[56px] h-[56px] items-center justify-center' : 'bg-transparent'} rounded-full p-2`}
-						style={{ backgroundColor: focused ? color : 'transparent' }}
-					>
-						<Plus
-							color={focused ? '#FFFFFF' : color}
-							size={size}
-						/>
-					</View>,
-					header: TabsHeader
-				}}
-			/>
+			{
+				['executor', 'encarregado'].includes(role.role_name) &&
+				<Tab.Screen
+					name="registerService"
+					component={RegisterServiceScreen}
+					options={{
+						title: 'Registrar',
+						tabBarLabel: ({ color, focused }) => focused ? '' : <Text style={{ color, fontSize: 10 }}>Registrar</Text>,
+						tabBarIcon: ({ color, size, focused }) => <View
+							className={`${focused ? 'w-[56px] h-[56px] items-center justify-center' : 'bg-transparent'} rounded-full p-2`}
+							style={{ backgroundColor: focused ? color : 'transparent' }}
+						>
+							<Plus
+								color={focused ? '#FFFFFF' : color}
+								size={size}
+							/>
+						</View>,
+						header: TabsHeader
+					}}
+				/>
+			}
 			<Tab.Screen
 				name="profile"
 				component={ProfileScreen}
